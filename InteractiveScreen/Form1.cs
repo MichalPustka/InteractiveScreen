42e25657420b2c379d64c2c6aea00cf3b9593d8b
Michal Pustka <michal.pustka@gmail.com>
2012-12-08 13:07:20 +0100

    new

--- InteractiveScreen/Form1.cs
+++ InteractiveScreen/Form1.cs
@@ -16,5 +16,12 @@ namespace InteractiveScreen
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ahoj svete");
+
+
+        }
     }
 }
